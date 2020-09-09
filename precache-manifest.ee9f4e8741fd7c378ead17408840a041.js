828a3864ebde9c86ee8c2f9ac464430010fdce25
Denff <manowardenff@gmail.com>
2020-09-09 09:26:26 +0500

    Updates

---
+++ precache-manifest.ee9f4e8741fd7c378ead17408840a041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63497d72044b898e47a083aa9f2a90c",
+    "url": "/react_weather/index.html"
+  },
+  {
+    "revision": "e8522d3c1ac49fbb5411",
+    "url": "/react_weather/static/css/main.37d5c01a.chunk.css"
+  },
+  {
+    "revision": "3147eb69566d28dd9d46",
+    "url": "/react_weather/static/js/2.3051c121.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_weather/static/js/2.3051c121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8522d3c1ac49fbb5411",
+    "url": "/react_weather/static/js/main.8e8e5e31.chunk.js"
+  },
+  {
+    "revision": "8f2b866f0bc02bfdde7b",
+    "url": "/react_weather/static/js/runtime-main.f864728f.js"
+  }
+]);
